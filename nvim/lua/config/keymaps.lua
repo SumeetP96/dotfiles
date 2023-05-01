38868f62138206fd0f0063409091bb5a1b470615
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2023-05-01 22:55:13 +0530

    autobackup 01-05-2023

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,26 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "K", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set({ "n", "v" }, "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gIc<Left><Left><Left><Left>]])
+
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+vim.keymap.set("n", "ss", ":split<Return><C-w>w")
+vim.keymap.set("n", "sv", ":vsplit<Return><C-w>w")
+
+vim.keymap.set("n", "<leader>tt", "<cmd>tabedit<CR>")
+vim.keymap.set("n", "tl", "<cmd>tabnext<CR>")
+vim.keymap.set("n", "th", "<cmd>tabprevious<CR>")
